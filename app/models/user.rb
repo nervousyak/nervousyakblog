6284eae55bf0dad8193eafc5230d502086438004
Kevin Lee <kevinwarrenlee@gmail.com>
2010-05-16 11:23:07 -0500

    Generated devise uer model, excluded confirmable stuff.

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :http_authenticatable, :token_authenticatable, :confirmable, :lockable, :timeoutable and :activatable, :validatable
+  devise :registerable, :authenticatable, :recoverable,
+         :rememberable, :trackable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation
+end
