dce427ef5a8a951736bf1545e36109b45aba1eb2
Kevin Lee <kevinwarrenlee@gmail.com>
2010-05-16 12:36:35 -0500

    added show action, and show view for blog

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,10 @@
 class BlogController < ApplicationController
   def index
-    
+    @articles = Article.all(:order => "created_at DESC", :limit => 5)
   end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
 end
