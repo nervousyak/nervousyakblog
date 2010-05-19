c798ce5c1411ad31781a21db1072b1ecfca929ef
Kevin Lee <kevinwarrenlee@gmail.com>
2010-05-18 21:25:36 -0500

    added redcloth and coderay, cleaned up the markup a bit

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,8 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "devise"
   config.gem "cancan"
+  config.gem "coderay"
+  config.gem "RedCloth"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
